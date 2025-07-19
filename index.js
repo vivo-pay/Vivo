57797f52353cf132fafe3d0d3fde765ba8d6bb01
vivo-pay <support@vivo-pay.com>
2025-07-19 17:34:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("VIVO backend is running!");
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('VIVO backend is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(Server is listening on port ${PORT});
+});
